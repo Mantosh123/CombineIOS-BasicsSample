1694a6e3fe5e0968bfbfed25b56ecec0bf3f8106
Mantosh Kumar <kumar.mantosh83@gmail.com>
2024-12-17 17:48:34 +0530

    Add map for print int value

--- SwiftCombine_Map/SwiftCombine_Map/ViewController.swift
+++ SwiftCombine_Map/SwiftCombine_Map/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import Combine
+
+struct FormField1 {
+    var value: Int
+}
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    var cancellables = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.textPublisher
+            .map({ value -> FormField1 in
+                FormField1(value: Int(value) ?? 0)
+            })
+            .sink { value in
+                print("value: \(value)")
+            }.store(in: &cancellables)
+        
     }
 
 
 }
 
+extension UITextField {
+    var textPublisher: AnyPublisher<String, Never> {
+        NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: self)
+            .compactMap { $0.object as? UITextField }
+            .map { $0.text ?? "" }
+            .eraseToAnyPublisher()
+    }
+}

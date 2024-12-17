185a485b307e1a32a56bdd6431af5989514319ee
Mantosh Kumar <kumar.mantosh83@gmail.com>
2024-12-17 17:18:34 +0530

    Add SwiftCombine_Map app code

---
+++ SwiftCombine_Map/SwiftCombine_Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftCombine_Map
+//
+//  Created by Mantosh Kumar on 17/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftCombine_Map/SwiftCombine_MapTests/SwiftCombine_MapTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCombine_MapTests.swift
+//  SwiftCombine_MapTests
+//
+//  Created by Mantosh Kumar on 17/12/24.
+//
+
+import Testing
+@testable import SwiftCombine_Map
+
+struct SwiftCombine_MapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
